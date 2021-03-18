02085157654ec749437bdb6f161cc1f6a84c8afe
ZzDamon <damonlepirda@gmail.com>
2021-03-18 14:20:41 +0200

    before starting DIP refactoring

---
+++ Week3/src/ase/cts/laboratory3/Stage2/ProductType.java
@@ -0,0 +1,15 @@
+package ase.cts.laboratory3.Stage2;
+
+public enum ProductType {
+	NEW(0), DISCOUNT(01.f), LIMITED_STOCK(0.25f), LEGACY(0.35f);
+	
+	private final float discount;
+	
+	private ProductType(float discount) {
+		this.discount=discount;
+	}
+	
+	public float getDiscount() {
+		return this.discount;
+	}
+}
